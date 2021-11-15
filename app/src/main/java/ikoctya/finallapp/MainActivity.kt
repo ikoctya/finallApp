f52445f22d41cd070cdf179c31d75815bc2c4a04
ikoctya <ikoctya@gmail.com>
2021-11-15 22:03:10 +0400

    0002

--- app/src/main/java/ikoctya/finallapp/MainActivity.kt
+++ app/src/main/java/ikoctya/finallapp/MainActivity.kt
@@ -2,10 +2,25 @@ package ikoctya.finallapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.title="ВАШ ЛЮБИМЫЙ ПРОВАЙДЕР"
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun ReplaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
 }
