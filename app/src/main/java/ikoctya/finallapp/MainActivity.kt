b54d315840ddb800285da10942a03cf76ad2a93b
ikoctya <ikoctya@gmail.com>
2021-11-15 20:40:56 +0400

    0001

---
+++ app/src/main/java/ikoctya/finallapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ikoctya.finallapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
